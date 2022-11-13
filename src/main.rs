5e73202b52206221f88b6d6d146d21f6dd3d05d0
xiaoshanshan <xiaoshanshan@xiaoshanshandeMacBook-Pro.local>
2022-11-13 15:06:03 +0800

    获取minidmp模块

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 mod read_dmp;
 
 
+
+
 fn main() {
-    println!("Hello, world!");
+   let mut dmp_item = read_dmp::ReadDmp{
+        dmp_path:"234".to_string(),
+   };
+   let dmp_result = dmp_item.get_dmp_module();
+   match dmp_result {
+    Ok(v) => println!("working with version: {v:?}"),
+    Err(e) => println!("error parsing header: {e:?}"),
+    }
 }
--- src/read_dmp.rs
+++ src/read_dmp.rs
@@ -1,23 +1,40 @@
-use minidump::Minidump;
+use minidump::*;
 use minidump_processor::{ProcessorOptions,Symbolizer};
-
+use anyhow::{self};
 #[derive(Debug, Clone)]
 pub struct DmpModule {
 //pdb信息
     pub debug_id: String,
     pub appendix :u32,
-    pub debug_identifier:String,
+    pub debug_file:String,
 //code信息
     pub code_identifier:String,
+//地址信息
+    pub start_address:u64,
+    pub end_address:u64
 }
 
 pub struct ReadDmp{
-    
+    pub dmp_path:String
 }
 
 
 impl ReadDmp {
-    pub fn get_dmp_module(){
-        
+    pub fn get_dmp_module(self)-> Result<Vec<DmpModule>,anyhow::Error> {
+        let mut dump = minidump::Minidump::read_path("test_data/test.dmp")?;
+        let mut module_lists = Vec::new();
+        if let Ok(modules) = dump.get_stream::<MinidumpModuleList>() {
+            for module_item in modules.iter(){
+                module_lists.push(DmpModule {
+                    debug_id:module_item.debug_identifier().unwrap_or_default().uuid().to_string(),
+                    appendix:module_item.debug_identifier().unwrap_or_default().appendix(),
+                    debug_file:module_item.debug_file().unwrap_or_default().to_string(),
+                    code_identifier:module_item.code_identifier().unwrap_or_default().to_string(),
+                    start_address:module_item.base_address(),
+                    end_address:module_item.base_address()+module_item.size(),
+                });
+            }
+        }
+        Ok(module_lists)
     }
 }
